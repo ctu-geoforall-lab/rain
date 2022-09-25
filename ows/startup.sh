34bfa6dd7172decdbc6a75971bc247e78de463bd
Martin Landa <landa.martin@gmail.com>
2022-09-25 23:49:24 +0200

    fix path

--- ows/startup.sh
+++ ows/startup.sh
@@ -48,7 +48,7 @@ while true; do
     LOGFILE=/var/log/rain_ows_tests-`date -Is`.log
     RETCODE=0
     echo "============================= WPS TESTS ==============================" > $LOGFILE
-    python3 -m pytest -x -v -s -o cache_dir=/tmp/pytest_cache_dir test_wps.py >> $LOGFILE
+    python3 -m pytest -x -v -s -o cache_dir=/tmp/pytest_cache_dir wps/test_wps.py >> $LOGFILE
     RETCODE=$((RETCODE+$?))
     python notifier.py $RETCODE $LOGFILE
     echo "Sleeping for ${OWS_TEST_REPEAT}hrs..."
