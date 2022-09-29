42d85aa2a04e2593b2fcb49c3d586f1a99065480
Martin Landa <landa.martin@gmail.com>
2022-09-29 18:29:32 +0200

    ows_test

--- ows/startup.sh
+++ ows/startup.sh
@@ -40,19 +40,4 @@ chgrp www-data /var/log/mapserv/mapserv.log
 gunicorn3 -b 0.0.0.0:8081 --workers $((2*`nproc --all`)) \
           --log-syslog  --pythonpath /opt/pywps pywps_app:application
 
-# run tests
-sleep 10
-while true; do
-    cd /opt/tests
-    echo "Performing tests..."
-    LOGFILE=/var/log/rain_ows_tests-`date -Is`.log
-    RETCODE=0
-    echo "============================= WPS TESTS ==============================" > $LOGFILE
-    python3 -m pytest -x -v -s -o cache_dir=/tmp/pytest_cache_dir wps/test_wps.py >> $LOGFILE
-    RETCODE=$((RETCODE+$?))
-    python notifier.py $RETCODE $LOGFILE
-    echo "Sleeping for ${OWS_TEST_REPEAT}hrs..."
-    sleep $(($OWS_TEST_REPEAT * 60 * 60))
-done
-
 exit 0
