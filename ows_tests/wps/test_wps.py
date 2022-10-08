055cd4b2b45466f4a714a07f9b8e39db8c3d293e
Martin Landa <landa.martin@gmail.com>
2022-10-08 12:40:42 +0200

    tests: back to rain1

--- ows_tests/wps/test_wps.py
+++ ows_tests/wps/test_wps.py
@@ -7,8 +7,8 @@ from owslib.wps import WebProcessingService, monitorExecution, ComplexDataInput
 from osgeo import ogr, gdal
 
 class TestWPS:
-    # url='https://rain1.fsv.cvut.cz/services/wps'
-    url='http://localhost/services/wps'
+    url='https://rain1.fsv.cvut.cz/services/wps'
+    # url='http://localhost/services/wps'
     input_data=ComplexDataInput("http://rain.fsv.cvut.cz/geodata/test.gml")
     key="HLGP_ID"
     return_period="N2,N5,N100"
