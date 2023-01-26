5a2722452e84f0519d8597756f2623fead8b6a9c
Martin Landa <landa.martin@gmail.com>
2023-01-26 16:59:16 +0100

    remove unused code

--- ows/wps/pywps/pywps_app.py
+++ ows/wps/pywps/pywps_app.py
@@ -9,17 +9,11 @@ from pywps.app.Service import Service
 os.environ['GISBASE'] = '/usr/lib/grass78' 
 sys.path.append(os.path.join(os.environ["GISBASE"], "etc", "python"))
 
-#from processes.subdayprecip_design_shp import SubDayPrecipShp
-#from processes.subdayprecip_design_csv import SubDayPrecipCsv
-#from processes.subdayprecip_design_point import SubDayPrecipPoint
 from processes.subdayprecip_design_shapes import SubDayPrecipShapes
 from processes.subdayprecip_design_shapes_total import SubDayPrecipShapesTotal
 from processes.soil_texture_hsg import SoilTextureHsgProcess
 
 processes = [
-#    SubDayPrecipShp(),
-#    SubDayPrecipCsv(),
-#    SubDayPrecipPoint(),
     SubDayPrecipShapes(),
     SubDayPrecipShapesTotal(),
     SoilTextureHsgProcess()
