d289fa8b6b7490f6a53e8a75df5731ebd888550d
Martin Landa <landa.martin@gmail.com>
2023-01-27 13:11:53 +0100

    empty template

---
+++ ows/wps/pywps/processes/cn_rain6h.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+from subprocess import PIPE
+
+from pywps import Process, ComplexInput, LiteralInput, ComplexOutput, Format, LOGGER
+from pywps.app.exceptions import ProcessError
+from pywps.validator.mode import MODE
+
+from grass.exceptions import CalledModuleError
+from grass.pygrass.modules import Module
+
+class CnRain6h(Process):
+    def __init__(self):
+        inputs = [
+        ]
+        outputs = [
+        ]
+
+        super(SoilTextureHsgProcess, self).__init__(
+            self._handler,
+            identifier="cn-rain6h",
+            version="1.0",
+            title="TBD",
+            abstract="TBD",
+            inputs=inputs,
+            outputs=outputs,
+            grass_location="/data/grassdata/TBD",
+            store_supported=True,
+            status_supported=True)
+
+        os.environ['GRASS_SKIP_MAPSET_OWNER_CHECK'] = '1'
+        os.environ['HOME'] = '/tmp' # needed by G_home()
+        
+    def _handler(self, request, response):
+        pass
--- ows/wps/pywps/pywps_app.py
+++ ows/wps/pywps/pywps_app.py
@@ -12,11 +12,13 @@ sys.path.append(os.path.join(os.environ["GISBASE"], "etc", "python"))
 from processes.subdayprecip_design_shapes import SubDayPrecipShapes
 from processes.subdayprecip_design_shapes_total import SubDayPrecipShapesTotal
 from processes.soil_texture_hsg import SoilTextureHsgProcess
+from processses.cn_rain6h import CnRain6h
 
 processes = [
     SubDayPrecipShapes(),
     SubDayPrecipShapesTotal(),
-    SoilTextureHsgProcess()
+    SoilTextureHsgProcess(),
+    CnRain6h()
 ]
 
 application = Service(
