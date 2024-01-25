bf2338ec3519a79c6d5e19aca303f41c1914f037
Martin Landa <landa.martin@gmail.com>
2024-01-25 14:13:58 +0100

    update hsg & udsa legend

--- ows/wms/utils/wms_legend.py
+++ ows/wms/utils/wms_legend.py
@@ -3,14 +3,20 @@
 import os
 from grass.pygrass.modules import Module
 
-def create_legend(rast, is_fp, output_dir='../legend'):
+def create_legend(rast, is_fp, output_dir='../legend', size=(71, 258)):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     Module('g.region', raster=rast)
-    Module('d.mon', start='cairo', width=71, height=258,
+    Module('d.mon', start='cairo', width=size[0], height=size[1],
             output=os.path.join(output_dir, rast.replace("_int", "") + '.png'),
             overwrite=True)
     Module('d.legend', raster=rast, at=[10,90,10,50],
-           flags="sf" if is_fp else "f")
+           flags="sf" if is_fp else "", font="DejaVuSans")
     Module('d.mon', stop='cairo')
+
+if __name__ == "__main__":
+    import sys
+    # usda 250x358
+    # hsg 150x200
+    create_legend(sys.argv[1], False, size=(sys.argv[2], sys.argv[3]))
