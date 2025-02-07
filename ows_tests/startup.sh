2e8212667cdac558b5f022868378f229dfb75b25
Martin Landa <landa.martin@gmail.com>
2025-02-07 20:15:21 +0100

    append WFS

--- ows_tests/startup.sh
+++ ows_tests/startup.sh
@@ -15,7 +15,7 @@ while true; do
     echo "============================= WMS TESTS ==============================" >> $LOGFILE
     python3 -m pytest -v -o cache_dir=/tmp/pytest_cache_dir ./tests/wms/test_wms.py >> $LOGFILE
     RETCODE=$((RETCODE+$?))
-    echo "============================= WFS TESTS ==============================" > $LOGFILE
+    echo "============================= WFS TESTS ==============================" >> $LOGFILE
     python3 -m pytest -v -o cache_dir=/tmp/pytest_cache_dir ./tests/wfs/test_wfs.py >> $LOGFILE
     RETCODE=$((RETCODE+$?))
     python notifier.py $RETCODE $LOGFILE
